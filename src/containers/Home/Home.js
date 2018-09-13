2d01640d452b8f43ca78fa92f1593efb5f65c52e
Jan Goebel <jan.goebel@in.tum.de>
2018-09-13 08:17:37 +0200

    adapt styling

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {VideoPreview} from '../../components/VideoPreview/VideoPreview';
+import './Home.css';
 
 export class Home extends React.Component {
   render() {
     return (
-      <div style={{padding: '400px'}}>
+      <div className='home'>
         <VideoPreview vertical={true} grid={true}/>
       </div>
     )
