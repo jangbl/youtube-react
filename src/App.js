a344e3db071756c971adcf33960af3ccd3ea68b2
Jan Goebel <jan.goebel@in.tum.de>
2018-09-16 22:05:15 +0200

    add react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 import {Home} from './containers/Home/Home';
 import {AppLayout} from './components/AppLayout/AppLayout';
+import {Route, Switch} from 'react-router-dom';
+import {Watch} from './containers/Watch/Watch';
 
 class App extends Component {
   render() {
     return (
       <AppLayout>
-        <Home/>
+        <Switch>
+          <Route path="/watch" component={Watch}/>
+          <Route path="/" component={Home}/>
+        </Switch>
       </AppLayout>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
+import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
