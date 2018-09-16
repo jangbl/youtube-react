aec089c3760c42d97f787df00f4933d4553f0d95
Jan Goebel <jan.goebel@in.tum.de>
2018-09-16 15:44:57 +0200

    add watch component skeleton

---
+++ src/containers/Watch/Watch.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Watch.css';
+
+export class Watch extends React.Component {
+  render() {
+    return (
+      <div>Watch component goes here</div>
+    );
+  }
+}
