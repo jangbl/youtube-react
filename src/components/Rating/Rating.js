6cdc325da4ff43d3edd32c36b75e173c34b059ad
Jan Goebel <jan.goebel.dev@gmail.com>
2018-10-06 10:04:22 +0200

    update rating

--- src/components/Rating/Rating.js
+++ src/components/Rating/Rating.js
@@ -3,14 +3,14 @@ import './Rating.scss';
 import {Icon, Progress} from "semantic-ui-react";
 
 export function Rating(props) {
-  const progress = props.likes && props.dislikes ? <Progress className='progress' percent={91} size='tiny'/> : null;
+  const progress = props.likes && props.dislikes ? <Progress percent={91} size='tiny'/> : null;
   return (
     <div className='rating'>
-      <div className='thumbs-up'>
+      <div >
         <Icon name='thumbs outline up'/>
         <span>{props.likes}</span>
       </div>
-      <div className='thumbs-down'>
+      <div >
         <Icon name='thumbs outline down'/>
         <span>{props.dislikes}</span>
       </div>
