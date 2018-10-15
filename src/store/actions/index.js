42bce64482ace8ed23b54e990645cd73a8c2e2e2
Jan Goebel <jan.goebel.dev@gmail.com>
2018-10-15 20:08:48 +0200

    add actions

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,16 @@
+export const REQUEST = 'REQUEST';
+export const SUCCESS = 'SUCCESS';
+export const FAILURE = 'FAILURE';
+export function createRequestTypes(base) {
+  if (!base) {
+    throw new Error('cannot create request type with base = \'\' or base = null');
+  }
+  return [REQUEST, SUCCESS, FAILURE].reduce((acc, type) => {
+    acc[type] = `${base}_${type}`;
+    return acc;
+  }, {});
+}
+
 export function createAction(type, payload = {}) {
   return {
     type,
