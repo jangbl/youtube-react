f988617ec55e28faa5fdbe299490adffacc70538
Jan Goebel <jan.goebel.dev@gmail.com>
2019-02-28 20:36:42 +0100

    update dependencies to latest versions

--- src/components/InfiniteScroll/InfiniteScroll.js
+++ src/components/InfiniteScroll/InfiniteScroll.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Waypoint from 'react-waypoint';
+import {Waypoint} from 'react-waypoint';
 import {Loader} from 'semantic-ui-react';
 import './InfiniteScroll.scss';
 
