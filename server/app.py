dbb8111c68e351760be6581e466465fdcec0356a
= <junruihu@gmail.com>
2021-03-07 15:24:36 -0500

    add option to specify video link

--- server/app.py
+++ server/app.py
@@ -17,11 +17,22 @@ def hello_world():
 
 @app.route('/bookmarks/', methods=['GET'])
 def get_bookmarks():
+
     query_parameters = request.args
+    video_link = query_parameters.get('video_link')
     object_name = query_parameters.get('object_name')
-    result = DUMMY_DICT.get(object_name, {})
+
+    result = None
+    if video_link:
+        if object_name:
+            result = DUMMY_DICT.get(object_name, {})
+        else:
+            result = DUMMY_DICT
+    else:
+        # use dummy return
+        result = DUMMY_DICT
     return jsonify(result)  # {} means not found!
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="127.0.0.1",port=5000)
